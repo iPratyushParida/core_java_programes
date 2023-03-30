f4fd39ae4046070f8c0d45fd83c7875fc7d2236f
Pratyush Kumar <Pratyush Kumar@SMTC-R-0172>
2023-03-30 10:31:11 +0530

    project uploaded today

---
+++ core_java_programes/src/core_java_programes/Demo.java
@@ -0,0 +1,9 @@
+package core_java_programes;
+
+public class Demo {
+
+	public static void main(String[] args) {
+System.out.println("hello world");
+	}
+
+}
---
+++ core_java_programes/src/star_pattern/Patteren1.java
@@ -0,0 +1,8 @@
+package star_pattern;
+
+public class Patteren1 {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
